22b57868e1f0b980db0bf191e6ed19fbc7a9561a
Aravindhan V <aravindhan.v@perfios.com>
2021-11-02 21:40:05 +0530

    ADDED: Implementation for employeePayrollService

--- src/main/java/com/bridgelabz/employeepayrollapp/services/IEmployeePayrollService.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/services/IEmployeePayrollService.java
@@ -1,5 +1,14 @@
 package com.bridgelabz.employeepayrollapp.services;
 
-public class IEmployeePayrollService {
+import java.util.List;
 
+import com.bridgelabz.employeepayrollapp.dto.EmployeePayrollDTO;
+import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
+
+public interface IEmployeePayrollService {
+	List<EmployeePayrollData> getEmployeePayrollData();
+	EmployeePayrollData getEmployeePayrollDataById(int empId);
+	EmployeePayrollData createEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+	EmployeePayrollData updateEmployeePayrollData(EmployeePayrollDTO empPayrollDTO);
+	void deleteEmployeePayrollData(int empId);
 }
