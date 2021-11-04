8588f44475f69048eb4d4c0dc6187ad6052ab18f
Aravindhan V <aravindhan.v@perfios.com>
2021-11-04 07:10:22 +0530

    ADDED: Custom Exception Hanlder for employee payroll

--- src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/dto/EmployeePayrollDTO.java
@@ -5,7 +5,7 @@ import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.Pattern;
 
 public class EmployeePayrollDTO {
-	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z\\s]{2,}$", message = "Employee name Invalid")
+	@Pattern(regexp = "^[A-Z]{1}[a-zA-Z	\\s]{2,}$", message = "Employee name Invalid")
 	public String name;
 	
 	@Min(value=500, message="Min wage should be more than 500")
