b4f181e93a7ce0ab685934f80b96f782eb11bbfb
Aravindhan V <aravindhan.v@perfios.com>
2021-11-08 07:00:04 +0530

    ADDED: custom query to get employee details by department

--- src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
+++ src/main/java/com/bridgelabz/employeepayrollapp/repository/EmployeePayrollRepository.java
@@ -1,9 +1,13 @@
 package com.bridgelabz.employeepayrollapp.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.bridgelabz.employeepayrollapp.model.EmployeePayrollData;
 
 public interface EmployeePayrollRepository  extends JpaRepository<EmployeePayrollData, Integer>{
-
+	@Query(value="select * from employee as e, department as d, employee_dept as ed where e.id=ed.emp_id and ed.dept_id=d.dept_id", nativeQuery = true)
+	List<EmployeePayrollData> findEmployeesByDepartment(String department);
 }
